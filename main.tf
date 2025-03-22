7bb3a642026f26269a1c0aac3fac91c07e05eaf3
Jamie Cummins <benocummins@outlook.com>
2025-03-21 22:44:40 -0400

    Initial commit - Terraform setup for Azure Rclone Offsite VM

---
+++ main.tf
@@ -0,0 +1,115 @@
+provider "azurerm" {
+    features{}
+    subscription_id                 = "7e2e2694-f97c-43f5-a145-f18c7823cbce"
+}
+
+resource "azurerm_resource_group" "rg" {
+    name                            = "rclone-offsite-rg"
+    location                        = "East US" 
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name                            = "rclone-offsite-vnet"
+    address_space                   = ["10.100.0.0/16"]
+    location                        = azurerm_resource_group.rg.location
+    resource_group_name             = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+    name                            = "rclone-offsite-subnet"
+    resource_group_name             = azurerm_resource_group.rg.name
+    virtual_network_name            = azurerm_virtual_network.vnet.name
+    address_prefixes                = ["10.100.10.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+    name                            = "rclone-offsite-public-ip"
+    location                        = azurerm_resource_group.rg.location
+    resource_group_name             = azurerm_resource_group.rg.name
+    allocation_method               = "Static"
+    sku                             = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+    name                            = "rclone-offsite-nic"
+    location                        = azurerm_resource_group.rg.location
+    resource_group_name             = azurerm_resource_group.rg.name
+
+    ip_configuration {
+        name                            = "internal"
+        subnet_id                       = azurerm_subnet.subnet.id
+        private_ip_address_allocation   = "Dynamic"
+        public_ip_address_id            = azurerm_public_ip.public_ip.id
+    }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+    name                            = "rclone-offsite-nsg"
+    location                        = azurerm_resource_group.rg.location
+    resource_group_name             = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                        = "Allow-SSH"
+        priority                    = 1000
+        direction                   = "Inbound"
+        access                      = "Allow"
+        protocol                    = "Tcp"
+        source_port_range           = "*"
+        destination_port_range      = "22"
+        source_address_prefix       = var.my_ip
+        destination_address_prefix  = "*"
+    }
+
+    security_rule {
+        name                        = "Allow-RDP"
+        priority                    = 1001
+        direction                   = "Inbound"
+        access                      = "Allow"
+        protocol                    = "Tcp"
+        source_port_range           = "*"
+        destination_port_range      = "3389"
+        source_address_prefix       = var.my_ip
+        destination_address_prefix  = "*"
+    }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+    network_interface_id           = azurerm_network_interface.nic.id
+    network_security_group_id       = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+    name                            = "rclone-offsite-vm"
+    resource_group_name             = azurerm_resource_group.rg.name
+    location                        = azurerm_resource_group.rg.location
+    size                            = "Standard_B1s" # Small and cheap
+    admin_username                  = "rcloneoffsiteadmin"
+
+    network_interface_ids           = [
+        azurerm_network_interface.nic.id,
+    ]
+
+    admin_ssh_key {
+        username              = "rcloneoffsiteadmin"
+        public_key                  = file("C:/Users/jamie/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching                     = "ReadWrite"
+        storage_account_type        = "Standard_LRS"
+    }
+
+    source_image_reference {
+        publisher                   = "Canonical"
+        offer                       = "0001-com-ubuntu-server-jammy"
+        sku                         = "22_04-lts-gen2"
+        version                     = "latest"
+    }
+    
+    custom_data = filebase64("${path.module}/cloud-init.yaml")
+}
+
+output "vm_public_ip" {
+    value                           = azurerm_public_ip.public_ip.ip_address
+    description                     = "The public IP address of the VM"
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "my_ip" {
+    description = "My public IP address with /32"
+    type        = string
+    default     = "97.75.150.102/32"
+  }
+  
