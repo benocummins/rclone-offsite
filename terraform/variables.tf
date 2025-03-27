41a2c5c162c3d01ce4552e713bd8cd604ccf14c6
Jamie Cummins <bocummins@icloud.com>
2025-03-26 20:54:04 -0400

    update variables... again... :D

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,27 @@ variable "my_ip" {
   type        = string
 }
 
-variable "public_ssh_key" {
+variable "rsa_public_key" {
   description = "The SSH public key to install on the VM"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
   type        = string
 }
