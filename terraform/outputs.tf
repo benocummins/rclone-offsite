ca7227e019e2e1797352775326f8e578c1e11c19
Jamie Cummins <bocummins@icloud.com>
2025-05-01 23:45:13 -0400

    Wrapped up the Ansible Action and Playbook. I updated Terraform to generate the Ansible Inventory file dynamically. The Discord message should output the public IP for reference on the actual build job.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,9 @@ output "vm_public_ip" {
     value       = azurerm_public_ip.public_ip.ip_address
     description = "The public IP address of the VM"
 }
+
+ouput "admin_username" {
+    value       = var.admin_username
+    description = "The administrator username for the VM"
+    sensitive   = false
+}
